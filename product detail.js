178dabf488dfcde9c933f41e3c3d1d96140b3377
Thiru7869 <131335194+Thiru7869@users.noreply.github.com>
2024-07-16 12:02:50 +0530

    Update product detail.js

--- product detail.js
+++ product detail.js
@@ -1,18 +1,16 @@
 import React from 'react';
-
-const ProductDetail = ({ product }) => {
-  return (
+const ProductDetail = ({ products }) => {
+  return products(
     <div className="product-detail">
       <img src={`https://picsum.photos/400/600?random=${product.id}`} alt={product.name} />
       <h1>{product.name}</h1>
-      <p>Company: {product.company}</p>
+      <p>Company: {product.companies}</p>
       <p>Rating: {product.rating}</p>
       <p>Price: ${product.price}</p>
-      <p>Discount: {product.discount}%</p>
+      <p>Discount: {product.discounts}%</p>
       <p>Availability: {product.availability ? 'In Stock' : 'Out of Stock'}</p>
       <p>Description: {product.description}</p>
     </div>
   );
 };
-
-export default ProductDetail;
+export default ProductsDetail;
